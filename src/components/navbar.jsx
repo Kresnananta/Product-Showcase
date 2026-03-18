7ceca8f261ff724adf55341f2990d8c9ff5962c7
Kresnananta <akresnaananta@gmail.com>
2026-03-18 14:12:01 +0700

    Initial Commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,34 @@
+import { navLinks } from '../constants/index';
+
+const Navbar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <img src="/logo.svg" alt="Logo" />
+
+          <ul>
+            {navLinks.map(({ label }) => (
+              <li key={label}>
+                <a href={label}>
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className='flex-center gap-3'>
+            <button>
+              <img src="/search.svg" alt="Search" width={18} height={18} />
+            </button>
+            <button>
+              <img src="/cart.svg" alt="Cart" width={18} height={18} />
+            </button>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+const navLinks = [
+    { label: "Store" },
+    { label: "Mac" },
+    { label: "Iphone" },
+    { label: "AirPods" },
+    { label: "Watch" }
+];
+
+export {
+    navLinks,
+}
